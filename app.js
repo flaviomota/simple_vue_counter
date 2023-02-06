6860e317c33a7146bf85c4862c631d9aa6a91623
Flávio Mota <fmota@student.42lisboa.com>
2023-02-06 17:20:08 +0000

    testing

--- app.js
+++ app.js
@@ -2,8 +2,27 @@ const app = Vue.createApp({
 	data() {
 		return {
 			counter: 0,
+			name: '',
+			confirmedName: '',
 		}
 	},
+	methods: {
+		add() {
+			this.counter++;
+		},
+		reduce() {
+			this.counter--;
+		},
+		setName(event, lastName) {
+			this.name = event.target.value + ' ' + lastName;
+		},
+		submitForm() {
+			alert('Submitted!');
+		},
+		confirmInput() {
+			this.confirmedName = this.name;
+		},
+	}
 })
 
 app.mount('#events');
